41e7139f2ba451d16f45de120a5efc15d970a952
Jan Sipr <jan.sipr@ixperta.com>
2016-08-22 15:49:54 +0200

    Add invocation methods

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,19 +3,52 @@ import { Reducer } from "redux";
 import React = __React;
 
 declare namespace ReactReduxToastr {
+    export interface ConfirmOptions {
+        confirtmTransitionIn: string,
+        confirtmTransitionOut: string,
+        confirtmOkText: string,
+        confirtmCancelText: string
+    }
+
     export interface ToastrProps {
         toastr: any
         options: any
         position: string,
         newestOnTop: boolean,
         timeOut: number,
-        confirmOptions: any
+        confirmOptions: ConfirmOptions
     }
 
     export class ReduxToastr extends React.Component<any, ToastrProps> {
     }
 
     export function reducer() : Reducer;
+
+    expoert interface BasicMsgOption {
+    icon: any
+        timeOut?: number,
+        onShowComplete?: any,
+        onHideComplete: any,
+        component: any,
+        removeOnClick?: boolean,
+        removeOnHover?: boolean,
+        /// 'top-right', 'top-left', 'bottom-right', ...
+        position: string,
+        newestOnTop: boolean,
+        timeOut: number
+
+    }
+
+    export class Toastr = {
+        function error(title?: string, msg: string, options?: any);
+        function info(title?: string, msg: string, options?: any);
+        function message (title?: string, msg: string, options?: any);
+        function success(title?: string, msg: strring, options?: any);
+        function warning(title?: string, msg: string, options?: any);
+        function confirm(mag: string, oprions?: any);
+
+        function clean();
+    }
 }
 
 export = ReactReduxToastr;
