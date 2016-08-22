9d1626cfac7e3aaaf7d18ca6bb88e3fbcb6cf713
Jan Sipr <jan.sipr@ixperta.com>
2016-08-22 14:19:47 +0200

    Learnig to make typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,16 +2,20 @@ import { Reducer } from "redux";
 
 import React = __React;
 
-export interface ToastrProps {
-    toastr: any
-    options: any
-    position: string,
-    newestOnTop: boolean,
-    timeOut: number,
-    confirmOptions: any
+declare namespace ReactReduxToastr {
+    export interface ToastrProps {
+        toastr: any
+        options: any
+        position: string,
+        newestOnTop: boolean,
+        timeOut: number,
+        confirmOptions: any
+    }
+
+    export class ReduxToastr extends React.Component<ToastrProps, any> {
+    }
+
+    export function reducer() : Reducer;
 }
 
-export class ReduxToastr extends React.Component<ToastrProps, any> {
-}
-
-export function reducer() : Reducer;
+export = ReactReduxToastr;
