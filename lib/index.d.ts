59176c063b6b4f58a02114377e0044d937e7f231
Jan Sipr <jan.sipr@ixperta.com>
2016-08-22 11:01:54 +0200

    Some test typings...

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,19 @@
-/**
- * This is just a template.
- * If the source does not export function at top level,
- * you can skip the declare namespace and export what's inside directly.
- */
 declare namespace react-redux-toastr {
+    import React = __React;
 
+    export interface ToastrProps {
+        toastr: any
+        options: any
+        position: string,
+        newestOnTop: boolean,
+        timeOut: number,
+        confirmOptions: any
+    }
+
+    export class ReduxToastr extends React.Component<ToastrProps, any> {
+    }
+
+    export function reducer() : Reducer;
 }
 
 export = react-redux-toastr;
